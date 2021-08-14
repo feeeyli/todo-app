b62e97a937ceecfb9977989ffc688e8d0ab9db9b
pedrohsfontoura <pedrohenrisantosfontoura@gmail.com>
2021-08-13 22:04:06 -0300

    :lipstick: style the NewTodo component

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import NewTodo from "./infra/inputs/NewTodo";
 import Header from "./infra/layout/Header";
 import "./sass/base.scss";
 
 ReactDOM.render(
 	<>
 		<Header />
+		<main>
+			<NewTodo />
+		</main>
 	</>,
 	document.getElementById("root")
 );
